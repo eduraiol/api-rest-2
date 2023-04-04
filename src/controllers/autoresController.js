ae77dd7092402cd1583d00e2d7dc2a9259c4ece0
Eduardo Raiol <marquesraiol@gmail.com>
2023-04-04 10:25:04 -0300

    Regatorando erros de buscas por ID de autores e livros

--- src/controllers/autoresController.js
+++ src/controllers/autoresController.js
@@ -1,3 +1,4 @@
+import NaoEncontrado from "../erros/NaoEncontrado.js";
 import autores from "../models/Autor.js";
 
 class AutorController {
@@ -17,10 +18,11 @@ class AutorController {
       const id = req.params.id;
 
       const autorResultado = await autores.findById(id);
+
       if(autorResultado !== null){
         res.status(200).send(autorResultado);
       }else{
-        res.status(404).send({message: "Id do Autor não localizado."});
+        next(new NaoEncontrado("Id do Autor não localizado."));
       }
     } catch (erro) {
       next(erro);
@@ -43,9 +45,13 @@ class AutorController {
     try {
       const id = req.params.id;
 
-      await autores.findByIdAndUpdate(id, {$set: req.body});
+      const autorResultado = await autores.findByIdAndUpdate(id, {$set: req.body});
 
-      res.status(200).send({message: "Autor atualizado com sucesso"});
+      if(autorResultado !== null){
+        res.status(200).send({message: "Autor atualizado com sucesso"});
+      }else {
+        next(new NaoEncontrado("Id o Autor não localizado."));
+      }   
     } catch (erro) {
       next(erro);
     }
@@ -55,9 +61,13 @@ class AutorController {
     try {
       const id = req.params.id;
 
-      await autores.findByIdAndDelete(id);
+      const autorResultado = await autores.findByIdAndDelete(id);
 
-      res.status(200).send({message: "Autor removido com sucesso"});
+      if(autorResultado !== null){
+        res.status(200).send({message: "Autor removido com sucesso"});
+      }else {
+        next(new NaoEncontrado("Id o Autor não localizado."));
+      }
     } catch (erro) {
       next(erro);
     }
--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,3 +1,4 @@
+import NaoEncontrado from "../erros/NaoEncontrado.js";
 import livros from "../models/Livro.js";
 
 class LivroController {
@@ -21,7 +22,12 @@ class LivroController {
         .populate("autor", "nome")
         .exec();
 
-      res.status(200).send(livroResultados);
+      if(livroResultados !== null){
+        res.status(200).send(livroResultados);
+      }else {
+        next(new NaoEncontrado("Id do Livro não localizado."));
+      }
+      
     } catch (erro) {
       next(erro);
     }
@@ -43,9 +49,15 @@ class LivroController {
     try {
       const id = req.params.id;
 
-      await livros.findByIdAndUpdate(id, { $set: req.body });
+      const livroResultado = await livros.findByIdAndUpdate(id, { $set: req.body });
 
-      res.status(200).send({ message: "Livro atualizado com sucesso" });
+      if(livroResultado !== null){
+        res.status(200).send({ message: "Livro atualizado com sucesso" });
+      }else {
+        next(new NaoEncontrado("Id do Livro não localizado."));
+      }
+
+      
     } catch (erro) {
       next(erro);
     }
@@ -55,9 +67,13 @@ class LivroController {
     try {
       const id = req.params.id;
 
-      await livros.findByIdAndDelete(id);
+      const livroResultado = await livros.findByIdAndDelete(id);
 
-      res.status(200).send({ message: "Livro removido com sucesso" });
+      if(livroResultado !== null){
+        res.status(200).send({ message: "Livro removido com sucesso" });
+      }else {
+        next(new NaoEncontrado("Id do Livro não localizado."));
+      }
     } catch (erro) {
       next(erro);
     }
@@ -69,12 +85,15 @@ class LivroController {
 
       const livrosResultado = await livros.find({ "editora": editora });
 
-      res.status(200).send(livrosResultado);
+      if(livrosResultado !== null) {
+        res.status(200).send(livrosResultado);
+      }else {
+        next(new NaoEncontrado("Nenhum Livro encontrado da editora fornecida."));
+      }
     } catch (erro) {
       next(erro);
     }
   };
-
 }
 
 export default LivroController;
