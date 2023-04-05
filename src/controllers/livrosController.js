71f3f49c696d90e4785812f75a999caccf34f920
Eduardo Raiol <marquesraiol@gmail.com>
2023-04-05 10:43:23 -0300

    reutilizando paginação com a criação do middleware paginar.js

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,33 +1,15 @@
 import NaoEncontrado from "../erros/NaoEncontrado.js";
-import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
 import { autores, livros } from "../models/index.js";
 
 class LivroController {
 
   static listarLivros = async (req, res, next) => {
     try {
+      const buscaLivros = livros.find();
 
-      let { limite = 5, pagina = 1, ordenacao = "_id:-1" } = req.query;
+      req.resultado = buscaLivros;
 
-      let [campoOrdenacao, ordem] = ordenacao.split(":");
-
-      limite = parseInt(limite);
-      pagina = parseInt(pagina);
-      ordem = parseInt(ordem);
-
-      if (limite > 0 && pagina > 0) {
-        const livrosResultado = await livros.find()
-          .sort({ [campoOrdenacao]: ordem })
-          .skip((pagina - 1) * limite)
-          .limit(limite)
-          .populate("autor")
-          .exec();
-
-        res.status(200).json(livrosResultado);
-      }else {
-        next(new RequisicaoIncorreta());
-      }
-
+      next();
     } catch (erro) {
       next(erro);
     }
@@ -103,11 +85,13 @@ class LivroController {
       const busca = await processaBusca(req.query);
 
       if (busca !== null) {
-        const livrosResultado = await livros
+        const livrosResultado = livros
           .find(busca)
           .populate("autor");
 
-        res.status(200).send(livrosResultado);
+        req.resultado = livrosResultado;
+
+        next();
       } else {
         res.status(200).send([]);
       }
